205ec3e91099a1cd745ccd06bd7ede5938b85114
Zoobop <86850448+Zoobop@users.noreply.github.com>
2022-09-27 00:09:39 -0500

    Update world transfer

--- Code/UpdateWorlds/Program.cs
+++ Code/UpdateWorlds/Program.cs
@@ -1,5 +1,20 @@
 ﻿
 
+static void CopyFilesRecursively(string sourcePath, string targetPath)
+{
+    //Now Create all of the directories
+    foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+    {
+        Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+    }
+
+    //Copy all the files & Replaces any files with the same name
+    foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+    {
+        File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+    }
+}
+
 var worldsDestinationDirectory = Environment.GetEnvironmentVariable("MinecraftWorlds");
 var worldsSourceDirectory = $@"{Directory.GetParent($"../../../")}\Worlds";
 var testOutputDirectory = $@"{Directory.GetParent($"../../../")}\TestOutput";
@@ -9,7 +24,7 @@ foreach (var world in worlds)
 {
     var index = world.LastIndexOf('\\');
     var name = world[++index..];
-    var destinationPath = $@"{worldsDestinationDirectory}\{name}";
-    Directory.Move(world, destinationPath);
+    var destinationPath = $@"{testOutputDirectory}\{name}";
+    CopyFilesRecursively(world, destinationPath);
 }
 
