7f4469388961f585b20442b4f8867e372444d801
Zoobop <86850448+Zoobop@users.noreply.github.com>
2022-09-27 00:00:11 -0500

    Added world transfer through code

---
+++ Code/UpdateWorlds/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+var worldsDestinationDirectory = Environment.GetEnvironmentVariable("MinecraftWorlds");
+var worldsSourceDirectory = $@"{Directory.GetParent($"../../../")}\Worlds";
+var testOutputDirectory = $@"{Directory.GetParent($"../../../")}\TestOutput";
+
+var worlds = Directory.GetDirectories(worldsSourceDirectory);
+foreach (var world in worlds)
+{
+    var index = world.LastIndexOf('\\');
+    var name = world[++index..];
+    var destinationPath = $@"{worldsDestinationDirectory}\{name}";
+    Directory.Move(world, destinationPath);
+}
+
